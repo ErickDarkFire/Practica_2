65406716c3d2136b7e704bc03c65edc13748a904
MrCooper <jorges.sanchez@iteso.mx>
2024-11-19 19:40:58 -0600

    Version_2

--- Snake.c
+++ Snake.c
@@ -2,9 +2,11 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-#define FRUIT_COLOR 0xFF0000  // Rojo
-#define SNAKE_COLOR 0x00FF00  // Verde
-#define EMPTY_COLOR 0x000000  // Negro
+// Jorge Sebastian Sanchez Arana | Erick Eduardo Rodriguez Gomez
+
+#define FRUIT_COLOR 0xFF0000 // Rojo
+#define SNAKE_COLOR 0x00FF00 // Verde
+#define EMPTY_COLOR 0x000000 // Negro
 
 // D-Pad para movimiento
 volatile unsigned int *d_pad_up = D_PAD_0_UP;
@@ -20,23 +22,27 @@ int board_width;
 int board_height;
 
 // Representación de la serpiente
-typedef struct {
+typedef struct
+{
     int x;
     int y;
 } Point;
 
-Point *snake;      // Lista dinámica de posiciones de la serpiente
-int snake_length;  // Longitud inicial
-Point fruit;       // Posición de la fruta
+Point *snake;     // Lista dinámica de posiciones de la serpiente
+int snake_length; // Longitud inicial
+Point fruit;      // Posición de la fruta
 
 // Variables de dirección
 int dir_x = 0;
 int dir_y = -1;
 
 // Inicializar tablero
-void draw_board() {
-    for (int y = 0; y < board_height; y++) {
-        for (int x = 0; x < board_width; x++) {
+void draw_board()
+{
+    for (int y = 0; y < board_height; y++)
+    {
+        for (int x = 0; x < board_width; x++)
+        {
             *(led_base + y * board_width + x) = EMPTY_COLOR;
         }
     }
@@ -45,11 +51,11 @@ void draw_board() {
     for (int i = 0; i < 2; i++)
     {
         *(led_base + fruit.y * board_width + fruit.x + i) = FRUIT_COLOR;
-        
     }
-    
+
     // Dibujar serpiente
-    for (int i = 0; i < snake_length; i++) {
+    for (int i = 0; i < snake_length; i++)
+    {
         *(led_base + snake[i].y * board_width + snake[i].x) = SNAKE_COLOR;
         // *(led_base + snake[i].y+1 * board_width + snake[i].x) = SNAKE_COLOR;
         // *(led_base + snake[i].y * board_width + snake[i].x+1) = SNAKE_COLOR;
@@ -57,30 +63,35 @@ void draw_board() {
         *(led_base + i * board_width) = SNAKE_COLOR;
     }
 
-    for (int i = 0; i < 8000; i++);
-    
+    for (int i = 0; i < 8000; i++)
+        ;
 }
 
 // Actualizar posición de la serpiente
-bool update_snake_position() {
+bool update_snake_position()
+{
     // Nueva cabeza
     int new_x = snake[0].x + dir_x;
     int new_y = snake[0].y + dir_y;
 
     // Validar colisión con bordes
-    if (new_x < 0 || new_y < 0 || new_x >= board_width || new_y >= board_height) {
+    if (new_x < 0 || new_y < 0 || new_x >= board_width || new_y >= board_height)
+    {
         return false; // Fin del juego
     }
 
     // Validar colisión con el cuerpo
-    for (int i = 0; i < snake_length; i++) {
-        if (snake[i].x == new_x && snake[i].y == new_y) {
+    for (int i = 0; i < snake_length; i++)
+    {
+        if (snake[i].x == new_x && snake[i].y == new_y)
+        {
             return false; // Fin del juego
         }
     }
 
     // Mover cuerpo: desplazar cada segmento al lugar del anterior
-    for (int i = snake_length - 1; i > 0; i--) {
+    for (int i = snake_length - 1; i > 0; i--)
+    {
         snake[i] = snake[i - 1];
     }
 
@@ -89,7 +100,8 @@ bool update_snake_position() {
     snake[0].y = new_y;
 
     // Detectar si come la fruta
-    if (new_x == fruit.x && new_y == fruit.y) {
+    if (new_x == fruit.x && new_y == fruit.y)
+    {
         snake_length++; // Incrementar longitud
         fruit.x = rand() % board_width;
         fruit.y = rand() % board_height;
@@ -99,23 +111,28 @@ bool update_snake_position() {
 }
 
 // Leer entradas del D-Pad
-void read_input() {
-    if (*d_pad_up && dir_y == 0) {
+void read_input()
+{
+    if (*d_pad_up && dir_y == 0)
+    {
         dir_x = 0;
         dir_y = -1;
         printf("Sube\n");
     }
-    if (*d_pad_down && dir_y == 0) {
+    if (*d_pad_down && dir_y == 0)
+    {
         dir_x = 0;
         dir_y = 1;
         printf("Baja\n");
     }
-    if (*d_pad_left && dir_x == 0) {
+    if (*d_pad_left && dir_x == 0)
+    {
         dir_x = -1;
         dir_y = 0;
         printf("Izquierda\n");
     }
-    if (*d_pad_right && dir_x == 0) {
+    if (*d_pad_right && dir_x == 0)
+    {
         dir_x = 1;
         dir_y = 0;
         printf("Derecha\n");
@@ -123,7 +140,8 @@ void read_input() {
 }
 
 // Configurar juego
-void setup_game(int n) {
+void setup_game(int n)
+{
     board_width = n;
     board_height = n;
 
@@ -139,20 +157,24 @@ void setup_game(int n) {
 }
 
 // Main del juego
-void main() {
+void main()
+{
     int n = 40; // Cambia este valor para modificar el tamaño del tablero
     setup_game(n);
 
-    while (1) {
+    while (1)
+    {
         read_input(); // Leer movimientos del usuario
-        if (!update_snake_position()) {
+        if (!update_snake_position())
+        {
             break; // Terminar el juego si hay colisión
         }
         draw_board(); // Dibujar el tablero
     }
 
     // Fin del juego
-    for (int i = 0; i < board_width * board_height; i++) {
+    for (int i = 0; i < board_width * board_height; i++)
+    {
         *(led_base + i) = 0xFF0000; // Rellenar matriz LED en rojo
     }
 
